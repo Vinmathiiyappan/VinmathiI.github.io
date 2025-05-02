0924670bb85a0d0e60cf6e891427fec057fe5909
Vinmathi Iyappan <vinmathi@Vinmathis-MacBook-Pro-5.local>
2025-05-02 12:08:01 -0700

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'https://darshanapi.vercel.app/api',
-    googleAnalyticsId: 'G-5MGTQMM8M5'
+    API_URL: 'https://portfolio-taupe-three-30.vercel.app/api',
+    googleAnalyticsId: 'G-P5H6RP3VT6'
 }
