d950891d547500cb06d5e43dd864538861b4173b
Vinmathi <126103032+Vinmathiiyappan@users.noreply.github.com>
2025-05-02 12:11:31 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'https://portfolio-taupe-three-30.vercel.app/api',
+    API_URL: 'https://www.vinmathi.site/api',
     googleAnalyticsId: 'G-P5H6RP3VT6'
 }
